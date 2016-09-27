7ac5c863436fedd5e8203bcd8a87a81178da3bef
Sergii Volodko <sergii.volodko@exact.com>
2016-09-27 20:52:13 +0200

    Net Core application added instead of WebApi

---
+++ Cyber-Fedya-Web/FedyaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Converters;
+
+namespace Cyber_Fedya_Api
+{
+    [Route("poehali")]
+    public class FedyaController: Controller
+    {
+        
+        [HttpGet("{primetext}")]
+        public string Poehali(string primetext)
+        {
+            return "Djiga-djiga!";
+        }
+    }
+}
