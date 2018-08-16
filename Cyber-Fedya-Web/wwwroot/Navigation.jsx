174109be767e7c3421a63e796c6b57c072ab1730
Sergii Volodko <sergii.volodko@exact.com>
2018-08-16 14:31:02 +0200

    Added bootstrap tabs for navigation

--- Cyber-Fedya-Web/wwwroot/Navigation.jsx
+++ Cyber-Fedya-Web/wwwroot/Navigation.jsx
@@ -14,9 +14,33 @@
 
     render() {
         return (
-            <div>Navigation
+            <div>
+                <ul class="nav nav-pills">
+                    <li class="active"><a data-toggle="pill" href="#generator">Поехали!</a></li>
+                    <li><a data-toggle="pill" href="#schemas">Схемы</a></li>
+                    <li><a data-toggle="pill" href="#dictinary">Словарь</a></li>
+                    <li><a data-toggle="pill" href="#history">Архив</a></li>
+                </ul>
 
+                <div class="tab-content">
+                    <div id="generator" class="tab-pane fade in active">
+                        <h3>Лэты гоу - поехали!</h3>
+                    </div>
+                    <div id="schemas" class="tab-pane fade">
+                        <h3>Схемы шуток</h3>
+                    </div>
+                    <div id="dictinary" class="tab-pane fade">
+                        <h3>Федин словарный запас</h3>
+                    </div>
+                    <div id="history" class="tab-pane fade">
+                        <h3>Сохранённые шедевры</h3>
+                    </div>
+                </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+       
     }
 }
